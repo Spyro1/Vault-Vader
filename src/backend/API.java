b6cc960fff2ca7b38592d1dfb51613301028b3ef
Spyro1 <redstone.mc01@gmail.com>
2024-09-10 18:43:11 +0200

    Frontend to Backend API class created

---
+++ src/backend/API.java
@@ -0,0 +1,61 @@
+package backend;
+
+import backend.categories.Category;
+import backend.fields.Field;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class API {
+
+    // == Login / Register Methods ==
+    static public boolean loginRequest(String username, String password) {
+       return false;
+    }
+    static public boolean registerRequest(String username, String password) {
+        return false;
+    }
+    // == Item Methods ==
+    static public boolean addItem(int itemId){
+        return false;
+    }
+    static public boolean modifyItem(int itemId, String title, Category category){
+        return false;
+    }
+    static public boolean removeItem(int itemId){
+        return false;
+    }
+    // == Category Methods ==
+    static public boolean addCategory(int categoryId, String name, Color color){
+        return false;
+    }
+    static public boolean modifyCategory(int categoryId, String name, Color color){
+        return false;
+    }
+    static public boolean removeCategory(int categoryId){
+        return false;
+    }
+    // == Item Field Methods ==
+    static public boolean addItemField(int itemId, int fieldId, int fieldType){
+        return false;
+    }
+    static public boolean modifyItemField(int itemId, int fieldId, String name){
+        return false;
+    }
+    static public boolean removeItemField(int itemId, int fieldId){
+        return false;
+    }
+
+    // == get methods ==
+    static public ArrayList<Item> getItemList(){
+        return null;
+    }
+    static public ArrayList<Category> getCategoryList(){
+        return null;
+    }
+    static public ArrayList<Field> getFieldList(){
+        return null;
+    }
+
+
+}
