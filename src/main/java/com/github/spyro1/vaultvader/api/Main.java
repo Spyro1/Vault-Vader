ad8c2af5f81b0973d0674fbbe1d00f9ec9ebe4de
Spyro1 <redstone.mc01@gmail.com>
2024-10-14 11:06:21 +0200

    Backend documented in code

--- src/main/java/com/github/spyro1/vaultvader/api/Main.java
+++ src/main/java/com/github/spyro1/vaultvader/api/Main.java
@@ -6,10 +6,15 @@ import javax.swing.*;
 
 public class Main {
 
-    static public void main(String[] args){
-        try{
+    /**
+     * The main method serves as the entry point of the application.
+     * @param args Command-line arguments (not used in this application).
+     */
+    public static void main(String[] args) {
+        try {
             new LoginUI(); // RUN LOGIN
-        } catch (Exception e){
+        } catch (Exception e) {
+            // Display a message dialog with the error in case of a fatal exception
             JOptionPane.showMessageDialog(null, e, "Fatal error", JOptionPane.ERROR_MESSAGE);
         }
     }
